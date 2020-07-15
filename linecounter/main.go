04f32fbed6d3a967d7351673ac0361c8f0c29d11
Ningze Dai <ningzedai@ningzes-mbp.lan>
2020-07-14 20:05:27 -0400

    line counter

--- linecounter/main.go
+++ linecounter/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world!")
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stdin, "Invalid number of arguments")
+		return
+	}
+	filename := os.Args[1]
+	file, err := os.Open(filename)
+	defer file.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stdin, "Cannot open file: %s\n", filename)
+		return
+	}
+	reader := bufio.NewReader(file)
+	numOfLines := 0
+	for {
+		_, isPrefix, err := reader.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Fprintf(os.Stdin, "Error while reading file: %v\n", err.Error())
+			break
+		}
+		// isPrefix will be set if the line is too long (> 4096 bytes)
+		if !isPrefix {
+			numOfLines++
+		}
+	}
+	fmt.Fprintf(os.Stdin, "There are %d lines in %s\n", numOfLines, filename)
 }
